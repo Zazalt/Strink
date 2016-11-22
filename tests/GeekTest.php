7b1b06285885aa25b1cb1acaced4233f0057fab7
Zazalt <liviu+zazalt+github.com@sindla.com>
2016-11-23 01:03:32 +0200

    The cake is a lie.

--- tests/GeekTest.php
+++ tests/GeekTest.php
@@ -10,4 +10,15 @@ class GeekTest extends \Zazalt\Strink\Tests\ZazaltTest
     {
         parent::loader($this, []);
     }
+
+    public function testClearSQLComments()
+    {
+        $stringsToTest = [
+            'SELECT * FROM TEST; --comment here' => 'SELECT * FROM TEST;'
+        ];
+
+        foreach($stringsToTest as $key => $value) {
+            $this->assertEquals($value, \Zazalt\Strink\Strink::turn($key)->clearSQLComments());
+        }
+    }
 }
